04924b8e47f7ea5279bbb42cdbcb3caeea4f5d7d
Diego Pertierra <dpertierra@hotmail.com>
2024-06-04 15:45:54 +0200

    Agrega validacion para la constante System::VERSION

--- LA BASE DE SKY/Data/Scripts/003_Technical/002_MKXP_Compatibility.rb
+++ LA BASE DE SKY/Data/Scripts/003_Technical/002_MKXP_Compatibility.rb
@@ -41,7 +41,7 @@ def pbSetResizeFactor(factor)
   end
 end
 
-if System::VERSION != Essentials::MKXPZ_VERSION
+if defined?(System::VERSION) && System::VERSION != Essentials::MKXPZ_VERSION
   printf(sprintf("\e[1;33mWARNING: mkxp-z version %s detected, but this version of Pokémon Essentials was designed for mkxp-z version %s.\e[0m\r\n",
                  System::VERSION, Essentials::MKXPZ_VERSION))
   printf("\e[1;33mWARNING: Pokémon Essentials may not work properly.\e[0m\r\n")
