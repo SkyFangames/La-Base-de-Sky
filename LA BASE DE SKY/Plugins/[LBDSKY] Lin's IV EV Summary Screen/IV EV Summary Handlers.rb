f944dad6669ecce6bdbd4aa7995a0e89698d827a
Diego Pertierra <dpertierra@hotmail.com>
2024-12-18 13:05:11 +0100

    Elimina codigo comentado

--- LA BASE DE SKY/Plugins/[LBDSKY] Lin's IV EV Summary Screen/IV EV Summary Handlers.rb
+++ LA BASE DE SKY/Plugins/[LBDSKY] Lin's IV EV Summary Screen/IV EV Summary Handlers.rb
@@ -5,7 +5,6 @@ UIHandlers.add(:summary, :page_allstats, {
   "name"      => "ESTADÍSTICAS",
   "suffix"    => "allstats",
   "order"     => 35,
-  # "options"   => [:item, :nickname, :pokedex, :mark],
   "condition" => proc { next Settings::SHOW_ADVANCED_STATS },
   "layout"    => proc { |pkmn, scene| scene.drawPageAllStats }
 })
