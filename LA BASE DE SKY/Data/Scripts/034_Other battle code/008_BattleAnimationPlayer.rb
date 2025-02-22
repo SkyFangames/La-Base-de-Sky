56339171c33348b281544714c9602ac9c59e3217
Diego Pertierra <dpertierra@hotmail.com>
2025-02-22 02:17:43 +0100

    correcciones animaciones

--- LA BASE DE SKY/Data/Scripts/034_Other battle code/008_BattleAnimationPlayer.rb
+++ LA BASE DE SKY/Data/Scripts/034_Other battle code/008_BattleAnimationPlayer.rb
@@ -482,19 +482,19 @@ class PBAnimation < Array
         begin 
           if bgGraphic.bitmap.nil?
             bgColor.opacity = oldbg[2] || 0 + ((i.opacity - (oldbg[2] || 0)) * fraction) if i.opacity
-            cr = i.colorRed ? oldbg[3]&.red || 0 + ((i.colorRed - (oldbg[3].red || Color.new(0, 0, 0, 0).red)) * fraction) : oldbg[3]&.red || Color.new(0, 0, 0, 0).red
-            cg = i.colorGreen ? oldbg[3]&.green || 0 + ((i.colorGreen - (oldbg[3]&.green || Color.new(0, 0, 0, 0).green)) * fraction) : oldbg[3]&.green || Color.new(0, 0, 0, 0).green
-            cb = i.colorBlue ? oldbg[3]&.blue || 0  + ((i.colorBlue - (oldbg[3]&.blue || Color.new(0, 0, 0, 0).blue)) * fraction) : oldbg[3]&.blue || Color.new(0, 0, 0, 0).blue
-            ca = i.colorAlpha ? oldbg[3]&.alpha || 0 + ((i.colorAlpha - (oldbg[3]&.alpha || Color.new(0, 0, 0, 0).alpha)) * fraction) : oldbg[3]&.alpha || Color.new(0, 0, 0, 0).alpha
+            cr = i.colorRed ? (oldbg[3]&.red || 0.0) + ((i.colorRed - (oldbg[3].red || Color.new(0, 0, 0, 0).red)) * fraction) : oldbg[3]&.red || Color.new(0, 0, 0, 0).red
+            cg = i.colorGreen ? (oldbg[3]&.green || 0.0) + ((i.colorGreen - (oldbg[3]&.green || Color.new(0, 0, 0, 0).green)) * fraction) : oldbg[3]&.green || Color.new(0, 0, 0, 0).green
+            cb = i.colorBlue ? (oldbg[3]&.blue || 0.0) + ((i.colorBlue - (oldbg[3]&.blue || Color.new(0, 0, 0, 0).blue)) * fraction) : oldbg[3]&.blue || Color.new(0, 0, 0, 0).blue
+            ca = i.colorAlpha ? (oldbg[3]&.alpha || 0.0) + ((i.colorAlpha - (oldbg[3]&.alpha || Color.new(0, 0, 0, 0).alpha)) * fraction) : oldbg[3]&.alpha || Color.new(0, 0, 0, 0).alpha
             bgColor.color = Color.new(cr, cg, cb, ca)
           else
             bgGraphic.ox      = (oldbg[0] || 0) - ((i.bgX - oldbg[0] || 0) * fraction) if i&.bgX
             bgGraphic.oy      = (oldbg[1] || 0) - ((i.bgY - oldbg[1] || 0) * fraction) if i&.bgY
             bgGraphic.opacity = (oldbg[2] || 0) + ((i.opacity - oldbg[2]) * fraction) if i&.opacity && oldbg[2]
-            cr = i.colorRed ? (oldbg[3]&.red || 0) + ((i.colorRed - (oldbg[3]&.red || 0)) * fraction) : oldbg[3]&.red || 0
-            cg = i.colorGreen ? (oldbg[3]&.green || 0) + ((i.colorGreen - (oldbg[3]&.green || 0)) * fraction) : oldbg[3]&.green || 0
-            cb = i.colorBlue ? (oldbg[3]&.blue || 0) + ((i.colorBlue - (oldbg[3]&.blue || 0)) * fraction) : oldbg[3]&.blue || 0
-            ca = i.colorAlpha ? (oldbg[3]&.alpha || 0) + ((i.colorAlpha - (oldbg[3]&.alpha || 0)) * fraction) : oldbg[3]&.alpha ||
+            cr = i.colorRed ? (oldbg[3]&.red || 0.0) + ((i.colorRed - (oldbg[3]&.red || 0.0)) * fraction) : oldbg[3]&.red || 0.0
+            cg = i.colorGreen ? (oldbg[3]&.green || 0.0) + ((i.colorGreen - (oldbg[3]&.green || 0.0)) * fraction) : oldbg[3]&.green || 0.0
+            cb = i.colorBlue ? (oldbg[3]&.blue || 0.0) + ((i.colorBlue - (oldbg[3]&.blue || 0.0)) * fraction) : oldbg[3]&.blue || 0.0
+            ca = i.colorAlpha ? (oldbg[3]&.alpha || 0.0) + ((i.colorAlpha - (oldbg[3]&.alpha || 0.0)) * fraction) : oldbg[3]&.alpha || 0.0
             bgGraphic.color = Color.new(cr, cg, cb, ca)
           end
         rescue NoMethodError
@@ -503,11 +503,11 @@ class PBAnimation < Array
       when 4
         begin
           if foGraphic.bitmap.nil?
-            foColor.opacity = (oldfo[2] || 0) + ((i&.opacity - (oldfo[2] || 0)) * fraction) if i&.opacity 
-            cr = i.colorRed ? (oldfo[3]&.red || 0) + ((i.colorRed - (oldfo[3]&.red || Color.new(0, 0, 0, 0).red)) * fraction) : oldfo[3]&.red || Color.new(0, 0, 0, 0).red
-            cg = i.colorGreen ? (oldfo[3]&.green || 0) + ((i.colorGreen - (oldfo[3]&.green || Color.new(0, 0, 0, 0).green)) * fraction) : oldfo[3]&.green || Color.new(0, 0, 0, 0).green
-            cb = i.colorBlue ? (oldfo[3]&.blue || 0) + ((i.colorBlue - (oldfo[3]&.blue || Color.new(0, 0, 0, 0).blue)) * fraction) : oldfo[3]&.blue || Color.new(0, 0, 0, 0).blue
-            ca = i.colorAlpha ? (oldfo[3]&.alpha || 0) + ((i.colorAlpha - (oldfo[3]&.alpha || Color.new(0, 0, 0, 0).alpha)) * fraction) : oldfo[3]&.alpha || Color.new(0, 0, 0, 0).alpha
+            foColor.opacity = (oldfo[2] || 0.0) + ((i&.opacity - (oldfo[2] || 0)) * fraction) if i&.opacity 
+            cr = i.colorRed ? (oldfo[3]&.red || 0.0) + ((i.colorRed - (oldfo[3]&.red || Color.new(0, 0, 0, 0).red)) * fraction) : oldfo[3]&.red || Color.new(0, 0, 0, 0).red
+            cg = i.colorGreen ? (oldfo[3]&.green || 0.0) + ((i.colorGreen - (oldfo[3]&.green || Color.new(0, 0, 0, 0).green)) * fraction) : oldfo[3]&.green || Color.new(0, 0, 0, 0).green
+            cb = i.colorBlue ? (oldfo[3]&.blue || 0.0) + ((i.colorBlue - (oldfo[3]&.blue || Color.new(0, 0, 0, 0).blue)) * fraction) : oldfo[3]&.blue || Color.new(0, 0, 0, 0).blue
+            ca = i.colorAlpha ? (oldfo[3]&.alpha || 0.0) + ((i.colorAlpha - (oldfo[3]&.alpha || Color.new(0, 0, 0, 0).alpha)) * fraction) : oldfo[3]&.alpha || Color.new(0, 0, 0, 0).alpha
             foColor.color = Color.new(cr, cg, cb, ca)
           else
             foGraphic.ox      = (oldfo[0] || 0) - ((i.bgX - (oldfo[0] || 0)) * fraction) if i&.bgX
