8386a6f498c47cfb4f624813823f9d2d1c7fdbc5
Diego Pertierra <dpertierra@hotmail.com>
2025-12-04 20:20:25 +0100

    Cambia orden de codigo

--- LA BASE DE SKY/Data/Scripts/030_Move/014_MoveEffects_SwitchingActing.rb
+++ LA BASE DE SKY/Data/Scripts/030_Move/014_MoveEffects_SwitchingActing.rb
@@ -1003,16 +1003,17 @@ class Battle::Move::UserMakeSubstituteSwitchOut < Battle::Move
       @battle.pbDisplay(_INTL("¡{1} ya está detrás de un sustituto!", user.pbThis))
       return true
     end
+    if !@battle.pbCanChooseNonActive?(user.index)
+      @battle.pbDisplay(_INTL("¡Pero no puede cambiar de Pokémon!"))
+      return true
+    end
     @lifeCost = [(user.totalhp / 2).ceil, 1].max
     @subLife = [(@lifeCost / 4).ceil, 1].max
     if user.hp <= @lifeCost
       @battle.pbDisplay(_INTL("¡Pero no tiene suficientes PS para hacer un sustituto!"))
       return true
     end
-    if !@battle.pbCanChooseNonActive?(user.index)
-      @battle.pbDisplay(_INTL("¡Pero no puede cambiar de Pokémon!"))
-      return true
-    end
+
     return false
   end
   
