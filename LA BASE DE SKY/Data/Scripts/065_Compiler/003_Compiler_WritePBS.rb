f7c0c7db9432829d19e8c8ae497c1f71b076ab74
Diego Pertierra <dpertierra@hotmail.com>
2026-02-20 15:50:27 +0100

    Corrige traduccion

--- LA BASE DE SKY/Data/Scripts/065_Compiler/003_Compiler_WritePBS.rb
+++ LA BASE DE SKY/Data/Scripts/065_Compiler/003_Compiler_WritePBS.rb
@@ -905,7 +905,7 @@ module Compiler
     write_dungeon_parameters
     write_phone
     echoln ""
-    Console.echo_h2(_INTL("Successfully rewrote all PBS files"), text: :green)
+    Console.echo_h2(_INTL("Se han reescrito correctamente todos los archivos PBS"), text: :green)
   end
 end
 
